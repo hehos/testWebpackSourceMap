616acb74c4eb40b4ed31a5c1e46a6f881ba55615
Lihuanghe <jameslover121@163.com>
2016-07-16 15:58:26 +0800

    add issue link and show error

--- entry.js
+++ entry.js
@@ -3,6 +3,6 @@ require('muicss');
 // mui 框架 js
 //必须在setTimeout里调用。否则样式出错
 //https://github.com/synchronizingtheworld/testWebpackSourceMap/issues/1
-setTimeout(function(){
+//setTimeout(function(){
 	window.mui = require('muijs');
-},0)
+//},0)
